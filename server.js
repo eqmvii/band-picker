eb83b8b1033f3f26be26d0053c05fc24a97f2b14
Eric (Martin) Mancini <coelomate@gmail.com>
2018-05-12 18:33:58 -0400

    passport boilerplate part 1

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+// Server
 var bodyParser = require("body-parser");
 var express = require("express");
 var express_handlebars = require("express-handlebars");
-
-// var Users = require("./models/user.js");
-
+// Auth
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+// Database
 var db = require("./models");
 
 
@@ -20,6 +22,31 @@ app.engine("handlebars", express_handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 
+//
+// AUTH
+//
+
+passport.use(new Strategy(
+    function (username, password, cb) {
+        db.User.findOne({ where: { username: username } }).then(user => {
+            if (!user) { return cb(null, false); }
+            if (user.password != password) { return cb(null, false); }
+            return cb(null, user);
+        });
+    }));
+
+passport.serializeUser(function (user, cb) {
+    cb(null, user.id);
+});
+
+passport.deserializeUser(function (id, cb) {
+    db.User.findOne({ where: { id: id } }).then(user => {
+        cb(null, user);
+    })
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //
 // ROUTES
